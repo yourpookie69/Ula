ff1ab08ba6ce5946a53d738aee56950cfcafd931
yourpookie69 <carloslopezmelgarejo70@gmail.com>
2025-09-24 02:33:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const range = document.getElementById('range');
 const statusText = document.getElementById('status');
 
 // Set the range to 100% permanently
-range.style.width = '100%';
+range.style.width = '75%';
 
 // Set the status to "OVERLOAD! CATCHING FIRE!" (you can change this to something else if you want)
 statusText.innerHTML = `Current Status: <strong style="color: darkred;">miss uuuu</strong>`;
+
 
 
